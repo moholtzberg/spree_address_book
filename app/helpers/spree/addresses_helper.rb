4368c1ddcca28c12457d2982dd14a7736c973055
Bryan Mahoney <bryan@goDynamo.com>
2012-08-21 09:16:05 -0400

    Resolves issue with state drop down not updating when country changes

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -1,6 +1,7 @@
 module Spree::AddressesHelper
   def address_field(form, method, id_prefix = "b", &handler)
-    content_tag :p, :id => [id_prefix, method].join('_'), :class => "field" do
+    id_prefix = id_prefix == 'bill_address' ? 'b' : 's'
+    content_tag :p, :id => [id_prefix, method].join(''), :class => "field" do
       if handler
         handler.call
       else
