94ae5b749be19976cce6a70d924eba4ed0915845
Bryan Mahoney <bryan@goDynamo.com>
2013-05-27 19:42:32 -0300

    Ensures the asterisk is added to the phone field

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -6,6 +6,7 @@ module Spree::AddressesHelper
         handler.call
       else
         is_required = Spree::Address.required_fields.include?(method)
+        is_required = true if method == :phone
         separator = is_required ? '<span class="req">*</span><br />' : '<br />'
         form.label(method) + separator.html_safe +
         form.text_field(method, :class => is_required ? 'required' : nil)
